3bb465f1c3e1e80a773a18176fe86a377b5b0c03
Takeshi1998 <t_fb0308@yahoo.co.jp>
2020-11-05 22:00:53 +0900

    修正

--- resources/views/str/update.blade.php
+++ resources/views/str/update.blade.php
@@ -1,31 +1,32 @@
-@component('components.header')
-@slot('title')
-編集
-@endslot
-@endcomponent
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                <a class="nav-link" href="./home">みんなの記録</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="./tweet">記録する</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">筋トレのすゝめ</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
-<!-- 一覧 -->
+  <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+   
+
+    <!-- Styles -->
+    <link href="{{asset('css/index.css')}}" rel="stylesheet">
+    
+  <title>みんなの記録</title>
+</head>
+<body>
+
 
 <main class="w-75 mx-auto mt-3 ">
     <form class="mt-4" method="post" action="./update/tweet">
         {{ csrf_field() }}
         <div class="form-group">
           <label for="tweet"></label>
-          <textarea class="form-control" id="tweet" rows="6" name="tweet">{!! nl2br(e($tweet->tweet))!!}</textarea>
+          <textarea class="form-control" id="tweet" rows="6" name="tweet">{!!e($tweet->tweet)!!}</textarea>
           
         </div>
       
@@ -39,3 +40,5 @@
 
       </form>
     </main>  
+  </body>
+  </html>
