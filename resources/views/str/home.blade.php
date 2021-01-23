778a6c5f6d2c05d3c42f419f163ec58078ca3706
d-take <take@d-plustrading.com>
2021-01-23 18:56:58 +0900

    font-size

--- resources/views/str/home.blade.php
+++ resources/views/str/home.blade.php
@@ -5,9 +5,6 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <!-- Scripts -->
-    {{-- <script src="{{ asset('js/app.js') }}" defer></script> --}}
-
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
