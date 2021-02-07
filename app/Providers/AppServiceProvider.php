b9ea941e7a117438934d255c4a5ffc26e5c46fc9
d-take <take@d-plustrading.com>
2021-02-07 12:34:32 +0900

    ssl化

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,8 +22,11 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
-        //
+        if ($this->app->environment() == 'production') {
+            $url->forceScheme('https');
+                 }
+
     }
 }
