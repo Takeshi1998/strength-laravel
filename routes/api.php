4b7a23be5ed0dd086d845442e8011f3cd8c8b15d
Takeshi1998 <t_fb0308@yahoo.co.jp>
2021-02-12 20:45:31 +0900

    apiに変更 line

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/line','LineBotController@callback')->name('Line.callback');
 
 
 
