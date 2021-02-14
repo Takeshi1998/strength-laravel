b635129cc6cbad9696bc849baec9f66adb36c43b
Takeshi1998 <t_fb0308@yahoo.co.jp>
2021-02-14 13:49:12 +0900

    o

--- app/Http/Controllers/LineBotController.php
+++ app/Http/Controllers/LineBotController.php
@@ -3,83 +3,52 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Database\Eloquent\Builder;
-use App\Comment;
-use App\User;
-use Carbon\Carbon;
-use DateTime;
-use DateTimeZone;
-use Exception;
+use App\Http\Controllers\Controller;
 use LINE\LINEBot;
 use LINE\LINEBot\Constant\HTTPHeader;
 use LINE\LINEBot\SignatureValidator;
 use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+use Exception;
 
-class LineBotController extends Controller
+class LineController extends Controller
 {
-    public function callback(Request $request){
+    public function webhook (Request $request)
+    {
+        $lineAccessToken = env('LINE_ACCESS_TOKEN', "");
+        $lineChannelSecret = env('LINE_CHANNEL_SECRET', "");
 
-        $httpClient = new CurlHTTPClient(env('LINE_ACCESS_TOKEN'));
-        $bot = new LINEBot($httpClient, ['channelSecret' =>env('LINE_CHANNEL_SECRET')]);
+        // 署名のチェック
         $signature = $request->headers->get(HTTPHeader::LINE_SIGNATURE);
-        try{
-            $events = $bot->parseEventRequest($request->getContent(), $signature);
-            foreach($events as $event){
-                $reply_token = $event->getReplyToken();
-                $message = $event->getText();
-                $textMessageBuilder = new TextMessageBuilder($message);
-                $response=$bot->replyMessage($reply_token, $textMessageBuilder);
-                echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
-                    }
-            }catch(Exception $e){
+
+        if (!SignatureValidator::validateSignature($request->getContent(), $lineChannelSecret, $signature)) {
+            // TODO 不正アクセス
             return;
         }
-        return ;
+
+        $httpClient = new CurlHTTPClient ($lineAccessToken);
+        $lineBot = new LINEBot($httpClient, ['channelSecret' => $lineChannelSecret]);
+
+        try {
+            // イベント取得
+            $events = $lineBot->parseEventRequest($request->getContent(), $signature);
+
+            foreach ($events as $event) {
+                // ログファイルの設定
+                // $file = __DIR__ . "/log.txt"
+                // file_put_contents($file, print_r($event, true) . PHP_EOL, FILE_APPEND);
+                // 入力した文字取得
+                $message = $event->getText();
+                $replyToken = $event->getReplyToken();
+                $textMessage = new TextMessageBuilder($message);
+                $lineBot->replyMessage($replyToken, $textMessage);
+            }
+        } catch (Exception $e) {
+            // TODO 例外
+            return;
+        }
+        return;
     }
-
-    public function message(){
-             // $now=new DateTime('now');
-        // $two_weeks_ago=$now->modify('-2 weeks');
-        // $now2=new DateTime('now');
-        // $four_days_ago=$now2->modify('-4 days');
-
-
-        // users 2週間以内にはしているが、4以内に筋トレをしてない人
-        // $users=User::whereHas('comments',function(Builder $query) use($two_weeks_ago){
-        //     $query->where('zikan','>',$two_weeks_ago);
-        // })->whereDoesntHave('comments', function (Builder $query) use($four_days_ago){
-        //     $query->where('zikan','>',$four_days_ago);
-        // });
-
-        // if($users->count()==0){
-        //     return ;
-        // }
-
-        // $userが>0のとき
-        // $names=[];
-        // foreach($users->get() as $user){
-        //     $names[]=$user->name;
-        // }
-        // $text=null;
-        // foreach($names as $name){
-        //     if($text==null){
-        //         $text=$name;
-        //     }else{
-        //         $text=$text.','.$name;
-        //     }
-        // }
-        // $text=$text.':筋トレの間隔空きすぎ';
-        // $receive = json_encode($text,JSON_UNESCAPED_UNICODE);
-
-        // dd($receive);
-        // $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(env('LINE_ACCESS_TOKEN'));
-        // $bot = new LINEBot($httpClient, ['channelSecret' =>env('LINE_CHANNEL_SECRET')]);
-        // $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($text);
-        // $response = $bot->pushMessage('<to>', $textMessageBuilder);
-        // echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
-
-        return ;
-    }
+}
 
 }
