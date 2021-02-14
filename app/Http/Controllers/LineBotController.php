f598e1e03ac0328e1c358d0abe5835bcaf81b213
Takeshi1998 <t_fb0308@yahoo.co.jp>
2021-02-14 15:28:11 +0900

    user id 取得

--- app/Http/Controllers/LineBotController.php
+++ app/Http/Controllers/LineBotController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Exception;
 use LINE\LINEBot;
 use LINE\LINEBot\Constant\HTTPHeader;
 use LINE\LINEBot\SignatureValidator;
@@ -18,21 +19,20 @@ class LineBotController extends Controller
         $lineAccessToken = env('LINE_ACCESS_TOKEN', "");
         $lineChannelSecret = env('LINE_CHANNEL_SECRET', "");
         $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($lineAccessToken );
-
-        //CurlHTTPClientとシークレットを使いLINEBotをインスタンス化
         $bot = new \LINE\LINEBot($httpClient, ['channelSecret' =>$lineChannelSecret]);
-
-        // LINE Messaging APIがリクエストに付与した署名を取得
-        $signature = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
-
-        //署名をチェックし、正当であればリクエストをパースし配列へ、不正であれば例外処理
-        $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
-
-        foreach ($events as $event) {
-            // メッセージを返信
-            $response = $bot->replyMessage(
-                $event->getReplyToken(), new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($event->getText())
-            );
+        try{
+            // LINE Messaging APIの署名を取得
+            $signature = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+            $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
+            foreach ($events as $event) {
+                $user_id=$event->source->userId;
+                $token=$event->getReplyToken();
+                $response = $bot->replyMessage(
+                    $token,new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($user_id)
+                );
+            }
+        }catch(Exception $e){
+            'エラー';
         }
     }
 
