5b97d51b77625f17b6e17215525e202e7e07e1a2
Takeshi1998 <t_fb0308@yahoo.co.jp>
2021-07-23 21:34:25 +0900

    [commemtout]

--- app/Http/Middleware/ForceHttps.php
+++ app/Http/Middleware/ForceHttps.php
@@ -15,9 +15,9 @@ class ForceHttps
      */
     public function handle($request, Closure $next)
     {
-        if (\App::environment(['production']) && $_SERVER["HTTP_X_FORWARDED_PROTO"] != 'https') {
-            return redirect()->secure($request->getRequestUri());
-          }
+        // if (\App::environment(['production']) && $_SERVER["HTTP_X_FORWARDED_PROTO"] != 'https') {
+        //     return redirect()->secure($request->getRequestUri());
+        //   }
         return $next($request);
     }
 }
