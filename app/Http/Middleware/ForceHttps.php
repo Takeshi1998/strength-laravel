93d0f55123a3203eb3152aed991eee6bdd3b6715
Takeshi1998 <t_fb0308@yahoo.co.jp>
2021-08-02 14:02:32 +0900

    [fix] https middlewarre

--- app/Http/Middleware/ForceHttps.php
+++ app/Http/Middleware/ForceHttps.php
@@ -15,9 +15,6 @@ class ForceHttps
      */
     public function handle($request, Closure $next)
     {
-        if (\App::environment(['production']) && $_SERVER["HTTP_X_FORWARDED_PROTO"] != 'https') {
-            return redirect()->secure($request->getRequestUri());
-          }
         return $next($request);
     }
 }
