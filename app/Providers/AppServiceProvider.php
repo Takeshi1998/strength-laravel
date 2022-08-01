940a750ae0b62aba2c653edbace35aa13977fbc0
Takeshi1998 <t_fb0308@yahoo.co.jp>
2022-08-01 22:26:28 +0900

    chore:http

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(UrlGenerator $url)
     {
         if ($this->app->environment() == 'production') {
-            $url->forceScheme('https');
+            $url->forceScheme('http');
                  }
 
     }
